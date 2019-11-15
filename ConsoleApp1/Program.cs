cbbad74778dc5e3191378f0cdb9a273401d4adf8
master <e>
2019-11-15 13:50:44 +0100

    asd

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLiteConnection conn = new SQLiteConnection("Data Source=db.db;Version=3");
+            conn.Open();
+
+            SQLiteCommand letrehoz = conn.CreateCommand();
+            letrehoz.CommandText = @"CREATE TABLE IF NOT EXISTS asd
+                                     (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                     nev VARCHAR(128) NOT NULL,
+                                     merete INTEGER NOT NULL);";
+            letrehoz.ExecuteNonQuery();
+
+
+        }
+    }
+}
